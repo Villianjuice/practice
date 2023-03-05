b3a05d36bbd833d70dd5c1910379105e4649ca5f
Aslan <tavorktar@gmail.com>
2023-03-05 21:45:22 +0600

    put method

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const Post = require('./models/post')
+const Contact = require('./models/contact')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
 const PORT = 3000;
+const db = 'mongodb+srv://Aslan:236393sr@cluster0.guezpkr.mongodb.net/node-block?retryWrites=true&w=majority'
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((res) => console.log('Connected to db'))
+    .catch((err) => console.log(err))
 
 const createPath = page => path.resolve(__dirname, 'ejs-views', `${page}.ejs`)
 
@@ -20,6 +30,8 @@ app.use(express.urlencoded({extended: false}))
 
 app.use(express.static('styles'))
 
+app.use(methodOverride('X-HTTP-Method-Override'))
+
 app.get('/', (req, res) => {
     const title = 'Home'
 
@@ -28,25 +40,26 @@ app.get('/', (req, res) => {
 
 app.get('/contacts', (req, res) => {
     const title = 'Contacts'
-    const contacts = [
-        { name: 'YouTube', link: 'http://youtube.com/YauhenKavalchuk' },
-        { name: 'Twitter', link: 'http://github.com/YauhenKavalchuk' },
-        { name: 'GitHub', link: 'http://twitter.com/YauhenKavalchuk' },
-    ];
-    res.render(createPath('contacts'), {contacts, title})
+    Contact
+        .find()
+        .then(contacts => res.render(createPath('contacts'), {contacts, title}))
+        .catch(err => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error' });
+        })
+    
 })
 
 app.post('/add-post', (req, res) => {
-    const {title, author, text} = req.body
-    const post = {
-        id: new Date(),
-        date: new Date().toLocaleDateString(),
-        title,
-        author, 
-        text,
-    }
-
-    res.render(createPath('post'), {post, title})
+    const { title, author, text } = req.body;
+    const post = new Post({ title, author, text });
+    post
+        .save()
+        .then((result) => res.redirect('/posts'))
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error' });
+        })
 })
 
 app.get('/add-post', (req, res) => {
@@ -55,33 +68,52 @@ app.get('/add-post', (req, res) => {
     res.render(createPath('add-post'), {title})
 })
 
+app.get('/edit/:id', (req, res) => {
+    const title = 'Edit post'
+    Post
+        .findById(req.params.id)
+        .then(post => res.render(createPath('edit-post'), {title, post}))
+        .catch(err => {
+            console.log(err)
+            res.render(createPath('error'), { title: 'Error' });
+        })
+})
+
 app.get('/posts/:id', (req, res) => {
     const title = 'Post'
-    const post = {
-        id: '1', 
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
-        title: 'Post title',
-        date: '05.05.2021',
-        author: 'Yauhen',
-      };
+    Post
+        .findById(req.params.id)
+        .then(post => res.render(createPath('post'), {title, post}))
+        .catch(err => {
+            console.log(err)
+            res.render(createPath('error'), { title: 'Error' });
+        })
+})
 
-    res.render(createPath('post'), {title, post})
+app.delete('/posts/:id', (req, res) => {
+    const title = 'Post'
+    Post
+        .findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.sendStatus(200)
+        })
+        .catch(err => {
+            console.log(err)
+            res.render(createPath('error'), { title: 'Error' });
+        })
 })
 
 app.get('/posts', (req, res) => {
     const title = 'Posts'
 
-    const posts = [
-        {
-            id: '1', 
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
-            title: 'Post title',
-            date: '05.05.2021',
-            author: 'Yauhen',
-        }
-    ]
-
-    res.render(createPath('posts'), {title, posts})
+    Post
+        .find()
+        .sort({createdAt: -1})
+        .then(posts => res.render(createPath('posts'), {title, posts}))
+        .catch(err => {
+            console.log(err)
+            res.render(createPath('error'), { title: 'Error' });
+        })
 })
 
 app.get('/about-us', (req, res) => {
